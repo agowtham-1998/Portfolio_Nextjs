99dcb30cc134e579a4027d28911b60ef93c8d52b
agowtham-1998 <agowtham8991@gmail.com>
2025-06-23 22:36:54 +0530

    styles fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-    "./src/**/*.{js,ts,jsx,tsx}",
-];
-export const theme = {
-    extend: {},
-};
-export const plugins = [];
+module.exports = {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
   
